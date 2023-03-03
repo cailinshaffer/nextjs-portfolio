42065623f891ccbbb6cba6bc5e680fe9201cd473
cailinshaffer <cailinshaffer@gmail.com>
2023-03-02 23:55:35 -0700

    adding resume and contact info

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,11 @@
 export default function Contact() {
     return(
-        <div>
+        <div className="contact">
             <h1>Contact Me </h1>
-            
+            <h4>Email: cailinshaffer@gmail.com</h4>
+            LinkedIn: <a href="https://www.linkedin.com/in/cailin-shaffer-5b1b1b1b9/">Cailin Shaffer</a>
+
+
         </div>
     )
 }
--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,13 @@
 export default function Resume() {
     return(
         <div>
-            <h1>Resume</h1>
+            <h1>Resume:</h1>
+            <div className='resume'>
+            <img 
+            src="/resume.png" 
+            alt="resume"
+             />
+            </div>           
             
         </div>
     )
