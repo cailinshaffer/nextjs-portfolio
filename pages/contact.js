9046b66443e05c79d92309b6329f57e5000fd3dc
cailinshaffer <cailinshaffer@gmail.com>
2023-03-04 12:32:19 -0700

    fixing linkedin link bug

--- pages/contact.js
+++ pages/contact.js
@@ -7,9 +7,10 @@ export default function Contact() {
     return(
         <div className={styles.container}>
             <h1>Contact Me </h1>
-            <h4>Email: cailinshaffer@gmail.com</h4>
-            <h4>LinkedIn: www.linkedin.com/in/cailin-shaffer </h4>
+            <h4>Email: cailinshaffer@gmail.com</h4> 
+            <h4>LinkedIn: </h4> <a href="https://www.linkedin.com/in/cailin-shaffer">Cailin Shaffer</a>
 
         </div>
+        
     )
 }
