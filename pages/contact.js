40be18f3243aaa96c09c4e32e345ae40b5b81b76
cailinshaffer <cailinshaffer@gmail.com>
2023-03-03 22:51:20 -0700

    home page

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,14 @@
+import styles from '../styles/Contact.module.css'
+import Link from 'next/link'
+
+
 export default function Contact() {
+
     return(
-        <div className="contact">
+        <div className={styles.container}>
             <h1>Contact Me </h1>
             <h4>Email: cailinshaffer@gmail.com</h4>
-            LinkedIn: <a href="https://www.linkedin.com/in/cailin-shaffer-5b1b1b1b9/">Cailin Shaffer</a>
-
+            <h4>LinkedIn: www.linkedin.com/in/cailin-shaffer </h4>
 
         </div>
     )
