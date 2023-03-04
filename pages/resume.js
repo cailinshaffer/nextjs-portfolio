1b65e920815e6e5beb3f0c733f4933583d593c97
cailinshaffer <cailinshaffer@gmail.com>
2023-03-04 12:22:37 -0700

    adding downloadable pdf resume file

--- pages/resume.js
+++ pages/resume.js
@@ -3,6 +3,14 @@ import styles from '../styles/Resume.module.css'
 export default function Resume() {
     return(
         <div className={styles.container}>
+            <div>
+              <a
+                 href="/resume.pdf"
+                alt="alt text"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Download Resume</a>
+            </div>
             <div className={styles.resume}>
             <img 
             src="/resume.png" 
